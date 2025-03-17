a4f860b0b93a153dcfee9e27e65561963de5b051
julian <cabralesjulian88@gmail.com>
2025-03-16 19:42:48 -0600

    11vo Commit

--- PIP_2025_Eq1/Unidad 2/P14_Cuaderno_de_Calificaciones.py
+++ PIP_2025_Eq1/Unidad 2/P14_Cuaderno_de_Calificaciones.py
@@ -12,13 +12,12 @@ datos = [[i[0],list(map(int, i[1:]))]for i in datos]
 
 print(datos)
 
-#investigar como calcular el promedio de cada alumno y agregar el resultado a la lista asociada al usuario
+
 
 datos = [[i[0],i[1], sum(i[1])/len(i[1])] for i in datos]
 print(datos)
 
-#Investigar como graficar valores de ese estilo (calificaciones) y ya después cómo hacerla en python
-#habia que sacar un histograma 12/02/2025
+
 
 nombres = [i[0] for i in datos]
 promedios = [i[2] for i in datos]
